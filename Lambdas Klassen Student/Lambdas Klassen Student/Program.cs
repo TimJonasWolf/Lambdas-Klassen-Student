cb03ea29733544f3e143b600ed829354256258ef
TimJonasWolf <132747647+TimJonasWolf@users.noreply.github.com>
2024-01-23 22:41:09 +0100

    Add files via upload

---
+++ Lambdas Klassen Student/Lambdas Klassen Student/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Lambdas_Klassen_Student;
+
+
+List<Student> liste = new List<Student>
+{
+	new Student {SNr=1,name="Awais"},
+
+	new Student {SNr=2,name="Vladi"},
+
+	new Student {SNr=3,name="Dejan"},
+
+	new Student {SNr=4,name="Browly"},
+
+	new Student {SNr=5,name="Zoro"},
+
+	new Student {SNr=6,name="Rick"}
+
+};
+
+foreach(Student student in liste.OrderBy(x=> x.name))
+{
+	Console.WriteLine(student.SNr+ " " + student.name);
+}
+Console.WriteLine();
+
+foreach (Student student in liste.OrderByDescending(x => x.SNr))
+{
+	Console.WriteLine(student.SNr + " " + student.name);
+}
+
+Console.WriteLine();
+
+var listeNew = from x in liste
+			  where x.name.Contains("B")
+			  select x;
+
+foreach (Student student in listeNew)
+{ 
+	Console.WriteLine(student.SNr + " " + student.name);
+}
+
+// FindAll,Select,OrderBy,OrderByDescending,from x in,where
+
+
---
+++ Lambdas Klassen Student/Lambdas Klassen Student/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas_Klassen_Student
+{
+	internal class Student
+	{
+		public int SNr { get; set; }
+		public string name { get; set; }
+	}
+}
